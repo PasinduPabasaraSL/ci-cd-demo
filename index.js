8e7f5c0016d9a28a84acfa2712d258b63494f5e2
Pasindu Pabasara <pabasarapasindu365@gmail.com>
2025-02-25 18:31:40 +0530

    Update message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('CI/CD demo');
+    res.send('Hello World');
 });
 
 if (require.main === module) {
